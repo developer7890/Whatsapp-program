1a283cab5def2c2e1d5324e6449ad6f4e0bcef70
developer7890 <41318448+developer7890@users.noreply.github.com>
2018-07-27 18:28:26 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,22 @@
+  $(document).ready(function() {
+     var myarray =[["https://pbs.twimg.com/profile_images/980832989994250240/NiVKoNZp_400x400.jpg","Yuvraj Singh 1"],
+                  ["https://pbs.twimg.com/profile_images/980832989994250240/NiVKoNZp_400x400.jpg","Yuvraj Singh 2"],
+                  ["https://pbs.twimg.com/profile_images/980832989994250240/NiVKoNZp_400x400.jpg","Yuvraj Singh 3"],
+                  ["https://pbs.twimg.com/profile_images/980832989994250240/NiVKoNZp_400x400.jpg","Yuvraj Singh 4"],
+                  ["https://pbs.twimg.com/profile_images/980832989994250240/NiVKoNZp_400x400.jpg","Yuvraj Singh 5"],
+                  ["https://pbs.twimg.com/profile_images/980832989994250240/NiVKoNZp_400x400.jpg","Yuvraj Singh 6"],
+                  ["https://pbs.twimg.com/profile_images/980832989994250240/NiVKoNZp_400x400.jpg","Yuvraj Singh 7"],
+                  ["https://pbs.twimg.com/profile_images/980832989994250240/NiVKoNZp_400x400.jpg","Yuvraj Singh 8"],
+                  ["https://pbs.twimg.com/profile_images/980832989994250240/NiVKoNZp_400x400.jpg","Yuvraj Singh 9"],
+                 ];
+                 var i;
+                  for(i=0;i<9;i++) {
+                    $(".middlelist").append("<li><img  src=" + myarray[i][0] + "><span>" + myarray[i][1] + "</span></li>");
+                  }
+
+                    $(".middlelist li").click(function() {
+                      var playername= $(this).text();
+                      var imageurl= $(this).find('img').prop('src');
+                      $.redirect("chatwindow.html", {name: encodeURIComponent(playername), url: imageurl}, "POST");
+                    });
+});
